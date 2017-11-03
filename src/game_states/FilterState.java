dc6fad1122d0e7d83a16eee131ec12fce696c12d
SeriousFresh <31888733+SeriousFresh@users.noreply.github.com>
2017-11-03 02:38:41 +0100

    sitan komit
    dodat txt sa korisnim linkom za filtere i 2 slike

---
+++ src/game_states/FilterState.java
@@ -0,0 +1,95 @@
+package game_states;
+
+import java.awt.Graphics2D;
+
+import rafgfxlib.GameHost;
+import rafgfxlib.GameHost.GFMouseButton;
+import rafgfxlib.GameState;
+
+public class FilterState extends GameState
+{
+
+	public FilterState(GameHost host)
+	{
+		super(host);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean handleWindowClose()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void resumeState()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void suspendState()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics2D g, int sw, int sh)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleMouseDown(int x, int y, GFMouseButton button)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleMouseUp(int x, int y, GFMouseButton button)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleMouseMove(int x, int y)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleKeyDown(int keyCode)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleKeyUp(int keyCode)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}

1aaacc8018fe306223c9f050620f858c9265e461
nvkvc <lenoagi@gmail.com>
2017-11-04 15:08:23 +0100

    -implemented SpriteSheet class and finished all player movements -implemented logic for player-field intersection
    It's only left to crop beer picture properly, and to merge mine and
    SeriousFresh's part

--- src/game_stage/Map.java
+++ src/game_stage/Map.java
@@ -1,6 +1,7 @@
 package game_stage;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.util.Random;
@@ -15,13 +16,21 @@ public class Map extends GameState{
 	public static final int MAP_WIDTH = 25;
 	public static final int MAP_HEIGHT =  25;
 	
-	public static final int PLYR_WIDTH = 128;
-	public static final int PLYR_HEIGHT = 128;
+	public static final int PLYR_WIDTH = 64;
+	public static final int PLYR_HEIGHT = 64;
 	
 	public static final int IMG_WIDTH = 64;
 	public static final int IMG_HEIGHT = 64;
 	
+	public static final int TERAIN_FIELDS = 9;
 	private static final int SPEC_FLD_NUM = 30;
+	
+	private static int PLYR_UP = 0;
+	private static int PLYR_DOWN = 0;
+	private static int PLYR_LEFT = 0;
+	private static int PLYR_RIGHT = 0;
+	
+	int moveCnt = 2;
 	
 	private static final String stateName = "maingamestate";
 	
@@ -29,69 +38,89 @@ public class Map extends GameState{
 	private Field specialField = null;
 	private Player player = null;
 	
+	private Field terainFields[] = new Field[9];
+	
 	private int shiftX = 0;
 	private int shiftY = 0;
 	int x0, x1, y0, y1;
 	int mdlX, mdlY;
 	int plyrVelX = 0;
 	int plyrVelY = 0;
+	int pos = 0;
 	
 	private GameHost host = null;
 	
-	private boolean map[][] = new boolean[MAP_HEIGHT][MAP_WIDTH];
-	
+	private int fields[][] = new int[MAP_HEIGHT][MAP_WIDTH];
+	private boolean isSpecial[][] = new boolean[MAP_HEIGHT][MAP_WIDTH];
 	private static int SHIFTX_MIN;
 	private static int SHIFTX_MAX;
 	private static int SHIFTY_MIN;
 	private static int SHIFTY_MAX;
 	
+	private BufferedImage playerUp[] = new BufferedImage[9];
+	private BufferedImage playerLeft[] = new BufferedImage[9];
+	private BufferedImage playerRight[] = new BufferedImage[9];
+	private BufferedImage playerDown[] = new BufferedImage[9];
 	
+	SpriteSheet plySS = null;
 	
 	public Map(GameHost host) {
 		super(host);
 		System.out.println(host.getWidth()+" "+host.getHeight());
 		this.host = host;
-		SHIFTX_MIN = -host.getWidth()/2+PLYR_WIDTH/2;
-		SHIFTX_MAX = IMG_WIDTH*MAP_WIDTH+SHIFTX_MIN-PLYR_WIDTH;
-		SHIFTY_MIN = -host.getHeight()/2+PLYR_HEIGHT/2;
-		SHIFTY_MAX = IMG_WIDTH*MAP_HEIGHT+SHIFTY_MIN-PLYR_HEIGHT;
+		initVariables();
 		initTerain();
 		initPlayer();
 	}
 	
+	public void initVariables(){
+		SHIFTX_MIN = -host.getWidth()/2+PLYR_WIDTH/2;
+		SHIFTX_MAX = IMG_WIDTH*MAP_WIDTH+SHIFTX_MIN-PLYR_WIDTH;
+		SHIFTY_MIN = -host.getHeight()/2-PLYR_HEIGHT/2+10;
+		SHIFTY_MAX = IMG_WIDTH*MAP_HEIGHT+SHIFTY_MIN-10;
+		mdlX = host.getWidth()/2;
+		mdlY = host.getHeight()/2;
+	}
 	public void initPlayer(){
 		BufferedImage img = null;
 		try{
-			img = Util.loadImage("/photos/dwarf.png");
+			img = Util.loadImage("/photos/charss.png");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		mdlX = host.getWidth()/2;
-		mdlY = host.getHeight()/2;
+		plySS = new SpriteSheet(img, 64);
+		playerUp = plySS.getRow(0, 9);
+		playerLeft = plySS.getRow(1, 9);
+		playerDown = plySS.getRow(2, 9);
+		playerRight = plySS.getRow(3, 9);
 		player = new Player(mdlX-PLYR_WIDTH/2, 
-							mdlY-PLYR_HEIGHT/2, img);
+							mdlY-PLYR_HEIGHT/2, playerDown[0]);
 	}
 	
 	public void initTerain(){
 		BufferedImage img = null;
 		try{
-			img = Util.loadImage("/photos/grass.png");
+			img = Util.loadImage("/photos/grass64p.png");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		classicField = new Field(IMG_WIDTH, IMG_HEIGHT, img, false);
-		try{
-			img = Util.loadImage("/photos/beer.png");
-		}catch(Exception e){
-			e.printStackTrace();
+		
+		terainFields[0] = new Field(IMG_WIDTH, IMG_HEIGHT, img, false);
+		for(int y = 0; y<MAP_HEIGHT; y++){
+			for(int x = 0; x<MAP_WIDTH; x++){
+				fields[y][x] = 0;
+				isSpecial[y][x] = false;
+			}
 		}
-		specialField = new Field(IMG_WIDTH, IMG_HEIGHT, img, true);
+	}
+	
+	public void initSpecialFields(){
 		Random r = new Random();
 		for(int i = 0; i < SPEC_FLD_NUM; ){
 			int x = r.nextInt(MAP_WIDTH);
 			int y = r.nextInt(MAP_HEIGHT);
-			if(!map[y][x]){
-				map[y][x] = true;
+			if(!isSpecial[y][x]){
+				isSpecial[y][x] = true;
 				i++;
 			}
 		}
@@ -114,11 +143,21 @@ public class Map extends GameState{
 		for(int y = y0; y<=y1; y++){
 			
 			for(int x = x0; x<=x1; x++){
-				
-				if(map[y][x]){
-					g.drawImage(specialField.getImage(), x*IMG_WIDTH-shiftX, y*IMG_HEIGHT-shiftY, null);
-				}else{
-					g.drawImage(classicField.getImage(), x*IMG_WIDTH-shiftX, y*IMG_HEIGHT-shiftY, null);
+				g.drawImage(terainFields[fields[y][x]].getImage(), x*IMG_WIDTH-shiftX, y*IMG_HEIGHT-shiftY, null);
+			}
+		}
+	}
+	
+	public void findIntersections(){
+		Rectangle plyrRect = new Rectangle(mdlX+shiftX-PLYR_WIDTH/2, mdlY+shiftY-PLYR_HEIGHT/2, PLYR_WIDTH, 
+				PLYR_HEIGHT);
+		Rectangle fldRect = null;
+		System.out.println("--------");
+		for(int y = 0; y<MAP_HEIGHT; y++){
+			for(int x = 0; x<MAP_WIDTH; x++){
+				fldRect = new Rectangle(x*IMG_WIDTH, y*IMG_WIDTH, IMG_WIDTH, IMG_HEIGHT);
+				if(plyrRect.getBounds().intersects(fldRect.getBounds())){
+					System.out.println(y + " " + x);
 				}
 			}
 		}
@@ -158,7 +197,7 @@ public class Map extends GameState{
 
 	@Override
 	public void update() {
-		player.updatePlayer(plyrVelX, plyrVelY);
+		//player.updatePlayer(plyrVelX, plyrVelY);
 		
 	}
 
@@ -182,10 +221,14 @@ public class Map extends GameState{
 
 	@Override
 	public void handleKeyDown(int keyCode) {
+		moveCnt = (moveCnt+1)%3;
 		if(keyCode == KeyEvent.VK_LEFT) {
 			shiftX -= 10;
 			if(shiftX<SHIFTX_MIN){
 				shiftX = SHIFTX_MIN;
+			}
+			if(moveCnt==0){
+				player.setImage(playerLeft[pos++]); 
 			}
 		}
 		if(keyCode == KeyEvent.VK_RIGHT) {
@@ -193,11 +236,19 @@ public class Map extends GameState{
 			if(shiftX>SHIFTX_MAX){
 				shiftX = SHIFTX_MAX;
 			}
+			
+			if(moveCnt==0){
+				player.setImage(playerRight[pos++]); 
+			}
 		}
 		if(keyCode == KeyEvent.VK_UP) {
 			shiftY -= 10;
 			if(shiftY<SHIFTY_MIN){
 				shiftY = SHIFTY_MIN;
+			}
+			
+			if(moveCnt==0){
+				player.setImage(playerUp[pos++]); 
 			}
 		}
 		if(keyCode == KeyEvent.VK_DOWN) {
@@ -206,9 +257,12 @@ public class Map extends GameState{
 				shiftY = SHIFTY_MAX;
 			}
 			
+			if(moveCnt==0){
+				player.setImage(playerDown[pos++]); 
+			}
 		}
 		
-		
+		pos %= 9;
 	}
 
 	@Override
@@ -219,8 +273,14 @@ public class Map extends GameState{
 		}
 		
 		if(keyCode == KeyEvent.VK_SPACE){
-			System.out.println("x0: "+x0+"\n"+"x1: "+x1+"\n"+"y0: "+y0+"\n"+"y1: "+y1+"\n"+"shiftX: "+shiftX+"\n"
-								+"shiftY: "+shiftY+"\n");
+			findIntersections();
+		}
+		
+		if(keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_LEFT
+			|| keyCode == KeyEvent.VK_RIGHT){
+			pos = 0;
+			moveCnt = 2;
+			player.setImage(playerDown[0]);
 		}
 	}
 	
